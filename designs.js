5f417f1eb507a315a2a04fc65d65e3d82d62b046
Matt <mcranfordmail@gmail.com>
2018-03-25 18:01:44 -0700

    Add jquery and get 3 user inputs

--- designs.js
+++ designs.js
@@ -1,5 +1,16 @@
+
+console.log("HEYYY!");
 // Select color input
+console.log($("#colorPicker").val());
+
 // Select size input
+console.log($("#inputHeight").val());
+console.log($("#inputWeight").val());
+
+// Submit button
+$("input[type='submit'").on("click",function() {
+  console.log("LISTEN!!");
+})
 
 // When size is submitted by the user, call makeGrid()
 
