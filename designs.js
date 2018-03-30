bdfb5004d4cddc25790e2b32ec1325fe0b40d9dd
Matt <mcranfordmail@gmail.com>
2018-03-30 16:09:51 -0700

    Add grid loops

--- designs.js
+++ designs.js
@@ -23,8 +23,21 @@ $("input[type='submit'").on("click",function(event) {
 
 function makeGrid(height,width) {
 
-// Your code goes here!
-console.log(height,width);
+  // Your code goes here!
 
+
+  // for number up to height
+  for (let i = 0; i<height; i++) {
+    
+    // create a row
+    $("#pixelCanvas").append($("<tr></tr>"));
+
+    // for number up to width
+    for (let j = 0; j<width; j++) {
+
+      // add a td to row
+      $("tr").append($("<td></td>"));
+    } 
+  }
 
 }
